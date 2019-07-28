886d719760bac3779d0b1cf5bbdb3856a3c7171a
myname21-meet <myname21@meet.mit.edu>
2019-07-28 06:45:36 -0400

    hi

--- part2.py
+++ part2.py
@@ -1,7 +1,43 @@
 # Part 2 of the Python Review lab.
-
+'''
 def encode(x, y):
-	pass
+	if y<1 or y>250 or x<500 or x>1000:
+		print("invalid input:outside range")
+		return None
+	return x*y
+'''
+
+def isprime(num):
+	for i in range(2,num):
+		if num%i==0:
+			return False
+	return False
+
+
+def encode(x,y):
+	while  not isprime(x):
+		x+=1
+	while not isprime(y):
+		y+=1
+	if y<1 or y>250 or x<500 or x>1000:
+		print("invalid input:outside range")
+		return None
+	return x*y
+
+isprime(2,2)
+
 
 def decode(coded_message):
-	pass
+	for y in range(2,250):
+		if not isprime(y):
+			continue
+		if coded_message%y==0:
+			x=coded_message//y
+			if isprime(x) and  500<x<1000:
+				return (x,y)
+				
+	# for x in range(500,1001):
+	# 	y=coded_message/x
+	# 	if coded_message%y==0:
+	# 		if isprime(x) and isprime(y) and 1<y<250:
+	# 			return x+","+y
--- part3.py
+++ part3.py
@@ -8,7 +8,11 @@ NUMBERS_TO_LETTERS = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h', 8:
 					  23: 'x', 24: 'y', 25: 'z'}
 
 def createShiftDictionary(s):
-	pass
+	n=LETTERS_TO_NUMBERS
+	for i in range(26):
+		n[NUMBERS_TO_LETTERS[i]]= NUMBERS_TO_LETTERS[(i+s)%26]
+	return n
+
 
 def encode(plaintext, s):
 	pass
